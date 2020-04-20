51f0efa7f2b03abdeb9b7c9fe3a9266fdac2b868
screwdrivercx <nai581305@gmail.com>
2020-04-20 11:31:00 +0700

    as

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('${__dirname}/front-end/dist/'));
+app.use(express.static(__dirname+'/src'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(`./front-end/dist/index.html`);
+  res.sendFile(__dirname+ '/src/index.html');
 });
 
 // Start the app by listening on the default Heroku port
