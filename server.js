595518a16cf7d6128328cab4aede20e54b890382
screwdrivercx <nai581305@gmail.com>
2020-04-20 11:57:01 +0700

    a

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname+'/src/app'));
+app.use(express.static(__dirname + '/dist/ToC'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(__dirname+ '/src/app/app.component.html');
+res.sendFile(path.join(__dirname+'/dist/ToC/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
